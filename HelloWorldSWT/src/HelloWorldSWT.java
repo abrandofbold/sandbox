c842b5548d1a3f9a4b665a8ee14a709e61402f92
Robert Ritter <rritter1@gmail.com>
2013-08-31 18:00:25 -0500

    Added truly global padding

--- HelloWorldSWT/src/HelloWorldSWT.java
+++ HelloWorldSWT/src/HelloWorldSWT.java
@@ -15,9 +15,9 @@ import static util.Calculating.*;
 public class HelloWorldSWT {
 
 	final static String longString = "The preferred size of a widget is the minimum size needed to show its content. In the case of a Composite, the preferred size is the smallest rectangle that contains all of its children. If children have been positioned by the application, the Composite computes its own preferred size based on the size and position of the children. If a Composite is using a layout class to position its children, it asks the Layout to compute the size of its clientArea, and then it adds in the trim to determine its preferred size.";
-
 	final static String disclaimer = "The results of this window have no way of limiting the redraw of the window to less than they can display, thus setting the width too small will overflow the text from visibility. This comment is no longer true as the limit has been resolved.";
-
+	final static int vPadding = 15;
+	
 	//Color bgColor = new Color(display,randInt(0, 255),randInt(0, 255),randInt(0, 255));
 	//shell.setBackground(bgColor);		
 	//shell.setText("Hello world!");
@@ -34,7 +34,6 @@ public class HelloWorldSWT {
 		
 		Listener listener = new Listener() {
 			public void handleEvent (Event event) {
-				int vPadding = 15;
 				
 				switch (event.type) {
 				case SWT.Paint:
