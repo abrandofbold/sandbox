2fab6aa5b7c627026397e51089de337ff11fca7b
Robert Ritter <rritter1@gmail.com>
2013-08-31 17:47:55 -0500

    Added variable minimum height for window

--- HelloWorldSWT/src/HelloWorldSWT.java
+++ HelloWorldSWT/src/HelloWorldSWT.java
@@ -16,7 +16,7 @@ public class HelloWorldSWT {
 
 	final static String longString = "The preferred size of a widget is the minimum size needed to show its content. In the case of a Composite, the preferred size is the smallest rectangle that contains all of its children. If children have been positioned by the application, the Composite computes its own preferred size based on the size and position of the children. If a Composite is using a layout class to position its children, it asks the Layout to compute the size of its clientArea, and then it adds in the trim to determine its preferred size.";
 
-	final static String disclaimer = "The results of this window have no way of limiting the redraw of the window to less than they can display, thus setting the width too small will overflow the text from visibility";
+	final static String disclaimer = "The results of this window have no way of limiting the redraw of the window to less than they can display, thus setting the width too small will overflow the text from visibility. This comment is no longer true as the limit has been resolved.";
 
 	//Color bgColor = new Color(display,randInt(0, 255),randInt(0, 255),randInt(0, 255));
 	//shell.setBackground(bgColor);		
@@ -42,6 +42,7 @@ public class HelloWorldSWT {
 				case SWT.Resize:
 					layout.setWidth(shell.getSize().x - 30);
 					layout2.setWidth(shell.getSize().x - 30);
+					shell.setMinimumSize(0,(layout.getLineCount()+layout2.getLineCount())*15+70);
 					break;
 				}
 			}
@@ -50,6 +51,7 @@ public class HelloWorldSWT {
 		shell.addListener(SWT.Paint, listener);
 		shell.addListener(SWT.Resize, listener);
 		shell.setMaximized(true);
+
 		shell.open();
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch())
