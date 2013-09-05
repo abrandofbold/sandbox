69ab58cada6caf596190cd25f24524d0153050e4
Robert Ritter <rritter1@gmail.com>
2013-09-04 23:20:36 -0500

    Add tie-game and estimate functionality

--- lu/src/LevelUp.java
+++ lu/src/LevelUp.java
@@ -7,8 +7,8 @@ import static java.lang.Math.*;
 public class LevelUp {
 
 	public static void main(String[] args) {
-		Player mike = new Player("Mike",35,4,49,27);
-		Player tony = new Player("Tony",14,9,37,40);
+		Player mike = new Player("Mike",14,10,23,14);
+		Player tony = new Player("Tony",17,11,20,40);
 		
 		p(compare(mike,tony).name+" estimated to win.");
 		
@@ -20,7 +20,6 @@ public class LevelUp {
 		Player a2 = new Player(a.name,a.atk,a.def,a.spd,a.hp);
 		Player b2 = new Player(b.name,b.atk,b.def,b.spd,b.hp);
 		
-		Player winner = null;
 		int time = 1;
 		
 		while(a2.hp > 0 && b2.hp > 0){
@@ -39,32 +38,17 @@ public class LevelUp {
 			p();
 		}
 		
-		if(a2.hp > b2.hp){
-			winner = a2;
-		}else{
-			winner = b2;
-		}
-		
-		return winner;
+		return (a2.hp > b2.hp || (a2.hp == b2.hp && a2.spd > b2.spd)) ? a2 : b2;
 	}
 
 	public static Player compare(Player a,Player b){
-		Player winner = null;
 		double scale;	
 		
-		scale = (((a.atk - b.def) / (double)b.hp) * a.spd) / (((b.atk - a.def) / (double)a.hp) * b.spd);
+		scale = ((max(a.atk - b.def,0) / (double)b.hp) * a.spd) / ((max(b.atk - a.def,0) / (double)a.hp) * b.spd);
 		
 		p(scale);
 		
-		if(scale < 0){
-			winner = a;
-		}else if(scale > 0){
-			winner = b;
-		}else{
-			
-		}
-		
-		return winner;
+		return (scale > 1 || (scale == 1 && a.spd > b.spd)) ? a : b;
 	}
 
 }
